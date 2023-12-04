3122188b1459b3671c9eb67974bbe7ef39fc87b2
ahmed <ahmed.welhazir8@gmail.com>
2023-12-04 20:36:43 +0100

    cors * vf

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const { Server } = require("socket.io");
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin:"*"
+  }
+});
 
 var corsOptions = {
   origin: "*"
